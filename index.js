129bc1f3f8bff014723346c2995594ef7ed274bf
kamran-pog <kamran.k0807@gmail.com>
2022-03-04 18:59:03 -0800

    finished MVP

--- index.js
+++ index.js
@@ -19,37 +19,49 @@ Do the following:
 
    HINT: no function required
 */
+const votingAge = 20
 
+if (votingAge >= 18){
+  console.log('true')
+} else {
+  console.log('Must be 18 or older to vote.')
+}
 
 
 /*
-Task 1b - Values (not auto tested)
-
+Task 1b - Values
 Do the following:   
-   1. Declare two variables and assign them values (good names for these might be firstThing and secondThing)
-   2. Use a conditional to check the value of the 1st variable versus the value assigned to the 2nd variable
-   3. Change the value of the 1st variable if the conditional in step 2 is true
-   4. Console log the value of the 1st variable
-
+   1. Declare two variables and assign them values
+   2. Use a conditional to change the value of the 1st variable based on the value assigned to the 2nd variable
+   3. Console log the new value of the 1st variable
    HINT: no function required
 */
 
+let var1 = 'var1'
+let var2 = 'var2'
 
+//console.log(var1)
+
+if (var2 === 'var2'){
+ var1 = 'var1 has changed'
+}
+//console.log(var1)
 
 
 
 /*
-Task 1c - Convert Strings to Numbers (not auto tested)
-
+Task 1c - Convert Strings to Numbers
 Do the following:   
-   1. Declare a variable with the string type value of "1999"
+   1. Declare a string type variable with the value of "1999"
    2. Convert the string value of "1999" to a integer value of 1999
    3. Console log the result
-
    HINT: look up the Number method
 */
 
-
+let string1999 = "1999"
+string1999 = Number(string1999)
+// console.log(string1999)
+// console.log(typeof string1999)
 
 
 /*
@@ -61,9 +73,10 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-  /*add your code here*/
+function multiply(num1, num2){
+  return num1*num2
 }
+console.log(multiply(3,4))
 
 
 
@@ -77,9 +90,11 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  age *= 7
+  return age
 }
+console.log(dogYears(3))
 
 
 
@@ -88,49 +103,50 @@ function dogYears(/*add your code here*/){
 //Dog feeder - Depending on their weight and age, we need to know how many pounds of food to feed our dog each day!
 /*
 Use the hungryDog function and feeding requirements below to do the following:
-1. Invoke the hungryDog function below and pass it a weight value in pounds, followed by an age value in years
-2. note: if the dog is a puppy, the age will be a decimal. For example: three months = 3/12 or .25
+1. Invoke the hungryDog function below and pass it a weight value in pounds
+2. Also, pass to hungryDog an age value in years (note: if the dog is a puppy, the age will be a decimal. For example: three months = 3/12 or .25)
 3. Do the proper calculations and return the number of pounds of raw food to feed the dog/puppy in a day
-
 REMEMBER: This program should handle correctly adult AND puppy ages and weights
-HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
-
 Feeding Requirements:
-
 Adult Dogs 1 year and older 
    up to 5 lbs - 5% of their body weight
    6 - 10 lbs - 4% of their body weight 
    11 - 15 lbs - 3% of their body weight 
    > 15lbs - 2% of their body weight 
-
 Puppies less than 1 year
    2 - 4 months 10% of their body weight
    4 - 7 months 5% of their body weight 
    7 - 12 months 4% of their body weight
   
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
-NOTE 2: This is a great time to check the tests to see what it expects, versus what is actually 
-        returned from your function. This is an example of the output to look for on each test point. 
-          ● hungryDogFunction › Dog is 1 year and is 5lbs or less
-
-            expect(received).toBe(expected) // Object.is equality
-
-            Expected: 0.2
-            Received: undefined
-
-              21 | describe('hungryDogFunction', ()=>{ 
-              22 |   it('Dog is 1 year and is 5lbs or less', ()=>{
-            > 23 |     expect(functions.hungryDog(4, 1)).toBe(0.2);
-                |
-          ^
-              24 |   })
-      
-        Notice the expected and received, expected is what the test is looking for, and received is what was actually returned from this function. You can also see it's passing in two values, the number 4 and the number 1. 
-        So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  let poundsOfFood
+  if (age >= (2/12) && age < (4/12)){ 
+    poundsOfFood = weight * .10
+    return poundsOfFood
+} else if (age >= (4/12) && age < (.583)){
+    poundsOfFood = weight * .05
+    return poundsOfFood
+} else if (age >= (.583) && age < 1){
+    poundsOfFood = weight * .04
+    return poundsOfFood
+} else if (age >= 1){
+    if (weight <= 5){
+      poundsOfFood = weight * .05
+      return poundsOfFood
+    } else if (weight > 6 && weight <= 10){
+      poundsOfFood = weight * .04
+      return poundsOfFood
+    } else if (weight > 11 && weight <= 15){
+      poundsOfFood = weight * .03
+      return poundsOfFood
+    } else if (weight > 15){
+      poundsOfFood = weight * .02
+      return poundsOfFood
+    }
+} 
 }
 
 
@@ -139,25 +155,57 @@ function hungryDog(/*add your code here*/){
 
 // Rock, Paper, Scissors - Let's play against the computer!
 /*
-Do the following:
-1. Create a new variable that randomly generates the computer's choice, this must not be done inside the function
-2. Use Math.random to determine the computer's choice (Math.random gives a random number between 0 and 1)
-3. Make a conditional that changes the variable to "rock", "paper", or "scissors" based on it's random number
-
+Create a global variable that randomly generates the computer's choice
+Use Math.random to determine the computer's choice (Math.random gives a random number between 0 and 1)
+NOTE: Computer must be a global variable. It will be passed into the function as an argument when the function is invoked. 
+HINT: While you can complete this with only conditionals based on strings, it may help to equate choice to a number when using Math.random()
 Use the game function below to do the following:
-1. Receive 2 parameters: a string with the user's choice of "rock", "paper", or "scissors" 
-   and the computer's choice of "rock", "paper", or "scissors".
-   Note: make sure the strings are all lower case or it will not pass the test
-2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match these strings below exactly.
+1. Receive 2 parameters: the user's choice and the computer's choice
+2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match the strings below exactly.
  - win should return "you win!"
  - lose should return "you lose!"
  - tie should return "it's a tie"
-
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
+HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 
+const computer = () => {
+  const num = Math.floor(Math.random() * 3)
+   switch(num){
+     case 0:
+       return 'rock';
+     case 1: 
+       return 'paper';
+     case 2: 
+       return 'scissors';
+   } return num
+ }
+
 function game(user, computer){
-  /*add your code here*/
+  if (user === computer) {
+    return `it's a tie`
+  } 
+  if (user === 'rock'){
+    if (computer === 'paper') {
+      return 'you lose!'
+    } else {
+      return 'you win!'
+    }
+  }
+  if (user === 'paper'){
+    if (computer === 'scissors'){
+      return 'you lose!'
+    } else {
+      return 'you win!'
+    }
+  }
+  if (user === 'scissors'){
+    if (computer === 'rock'){
+      return 'you lose!'
+    } else {
+      return 'you win!'
+    }
+}
 }
 
 
@@ -173,8 +221,9 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilometers){
+  kilometers *= 0.621371
+  return kilometers
 }
 
 
@@ -186,11 +235,10 @@ Using the feet function below do the following:
 2. Convert the number of cm to feet
 3. Return number of feet
 */
-
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  cm /= 30.48
+  return cm
 }
-
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -207,9 +255,8 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
-}
+function annoyingSong(startingNum){
+      return `${startingNum} bottles of soda on the wall, ${startingNum} bottles of soda, take one down pass it around ${startingNum - 1} bottles of soda on the wall`
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -227,9 +274,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if (score >= 90 && score <=100){
+    return 'you got an A'
+  } else if (score >= 80 && score <=89){
+    return 'you got a B'
+  } else if (score >= 70 && score <=79){
+    return 'you got a C'
+  } else if (score >= 60 && score <=69){
+    return 'you got a D'
+  } else if (score < 50){
+    return 'you got an F'
+  }
 }
+
 
 
 
@@ -269,4 +327,4 @@ module.exports = {
   feet,
   annoyingSong,
   grade
-}
+}}
